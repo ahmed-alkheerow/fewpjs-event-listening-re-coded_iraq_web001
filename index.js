cff74e7a978aa4249de4e5dc2fa44a789d61abad
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-18 00:52:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ function addingEventListener() {
 
 
 const input = document.getElementById('input');
+input.addEventListener('mousemove', function(event){
+  alert('you moved one me')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
